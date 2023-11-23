ee52240c642adbf838a744ae80bcb7de64f105e6
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2023-11-23 22:16:30 +0100

    Add layout

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,20 @@ function App() {
       </div>
       <div className="body">
         <div className="variables">
+          <form>
+            <div>
+              <label htmlFor="foo">foo: </label>
+              <input type="text" name="foo" id="foo" required />
+            </div>
+            <div>
+              <label htmlFor="bar">bar: </label>
+              <input type="text" name="bar" id="bar" required />
+            </div>
+          </form>
         </div>
         <div className="file">
           <div className="template">
+            <textarea>It was a dark and stormy night...</textarea>
           </div>
           <div className="preview">
             <CodeBlock content={Mustache.render(sampleCode, {bar: "foo bar"})} />
